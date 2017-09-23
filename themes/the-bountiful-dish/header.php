5c0cefec739f401279cecfb5990beb148dfc402c
David Olson <david.g.olson.91@gmail.com>
2017-09-23 10:04:45 -0400

    Theme header

--- themes/the-bountiful-dish/header.php
+++ themes/the-bountiful-dish/header.php
@@ -5,7 +5,7 @@
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
+ *;
  * @package the-bountiful-dish
  */
 
@@ -24,33 +24,24 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'the-bountiful-dish' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1>New Site Title</h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'the-bountiful-dish' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<header class="header" id="header">
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="large-2 cell">
+					<a href="/" alt="The Bountiful Dish"><?php get_template_part('assets/img/logo'); ?></a>
+				</div>
+				<div class="large-10 cell">
+					<ul class="menu">
+						<li><a href="/">Home</a></li>
+						<li><a href="/menu">This Week's Menu</a></li>
+						<li><a href="/our-mission">Our Mission</a></li>
+						<li><a href="/catering">Catering</a></li>
+						<li><a href="/faqs">FAQs</a></li>
+						<li><a href="/blog">Blog</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
 
 	<div id="content" class="site-content">
