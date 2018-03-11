9f49a8a6d9d6c179fa3629510792318322083a8d
David Olson <david.g.olson.91@gmail.com>
2018-03-11 15:32:06 -0400

    Update homepage and footer

--- themes/the-bountiful-dish/footer.php
+++ themes/the-bountiful-dish/footer.php
@@ -69,7 +69,21 @@
 	</section>
 </div><!-- #page -->
 
+
+
 <?php wp_footer(); ?>
+
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-43440877-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-43440877-1');
+</script>
 
 </body>
 </html>
